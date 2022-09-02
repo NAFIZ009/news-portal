a8ba0249486fb0d2a621bf416beca74068cb16d2
NAFIZ009 <jalalnafiz3@gmail.com>
2022-09-03 02:40:16 +0600

    s

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const gettingCategory=()=>{
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then((res) => res.json())
-    .then((data) =>placingCategory(data.data.news_category))
+    .then((data) =>placingCategory(data))
 }
 gettingCategory();
 
-const placingCategory =(catagories)=>{
+const placingCategory =(data)=>{
+    let catagories=data.data.news_category;
     const catagotryContainer=document.getElementById("catagotryContainer");
     let childContainer = document.createElement("div");
     childContainer.className='row gap-3';
@@ -13,9 +14,30 @@ const placingCategory =(catagories)=>{
         let colContainer = document.createElement("div");
         colContainer.className="col py-2 text-center ";
         colContainer.innerHTML=`
-        <button class="bg-transparent border-0" onclick="">${catagory.category_name}</button>
+        <button class="bg-transparent border-0" onclick="gettingNews(event,'${catagory.category_id}','${catagory.category_name}')">${catagory.category_name}</button>
         `;
         childContainer.appendChild(colContainer);
     });
     catagotryContainer.appendChild(childContainer);
+}
+
+const gettingNews=(e,id,catName)=>{
+    // e.target.classList.toggle("activeBtn");
+    fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
+    .then((res) => res.json())
+    .then((data) =>displayNewsPart(data,catName))
+}
+
+const displayNewsPart=(data,catName) =>{
+    const news=data.data;
+    const categoryName=catName;
+    console.log(news);
+    displayItemCountContainer(news,categoryName);
+}
+
+const displayItemCountContainer=(news,categoryName)=>{
+    const container=document.getElementById("itemCountContainer");
+    container.innerHTML=`
+    <p class="ps-3 fw-bolder fs-4 py-3 bg-light rounded">${news.length} Itmes Founds For The Category ${categoryName}</p>
+    `;
 }
